a5d48a62e26e60be9971314f8e326e0d7e4daae3
karis-f <karis@bcicentral.com>
2024-02-22 05:28:26 +0700

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -8,8 +8,11 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      reporter: "mochawesome",
-        (config.specPattern = ["cypress/e2e/1-Belajar/Homepage.cy.js"]);
+      require("cypress-mochawesome-reporter/plugin")(on);
+      config.specPattern = [
+        // "cypress/e2e/1-Belajar/Homepage.cy.js",
+        // "cypress/e2e/1-Belajar/....cy.js",
+      ];
       return config;
     },
     viewportHeight: 768,
