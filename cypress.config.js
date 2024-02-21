033a10d1386a513df419ac4f9dcdd9e343a5d596
adindaputrinr <novkarizky@gmail.com>
2024-02-21 22:29:47 +0700

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporter: "reporters/custom.js",
+  pageLoadTimeout: 60000,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      reporter: "mochawesome",
+        (config.specPattern = ["cypress/e2e/1-Belajar/Homepage.cy.js"]);
+      return config;
     },
+    viewportHeight: 768,
+    viewportWidth: 1500,
+    experimentalStudio: true,
   },
 });
